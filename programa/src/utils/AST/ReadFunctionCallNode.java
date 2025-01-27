fb85b2bfe515c4d988a3126fc7d84a04161a936a
Joselyn Jiménez <pjoselyn2114@gmail.com>
2025-01-26 20:59:20 -0600

    Avance y mejorar en Estructuras AST:  Print, Read, Return

--- programa/src/utils/AST/ReadFunctionCallNode.java
+++ programa/src/utils/AST/ReadFunctionCallNode.java
@@ -1,11 +1,12 @@
 
 package utils.AST;
 
-public class PrintFunctionCall extends ASTNode {
+
+public class ReadFunctionCallNode extends ASTNode {
     
     ExpressionNode expression;
     
-    public PrintFunctionCall(ExpressionNode expression){
+    public ReadFunctionCallNode(ExpressionNode expression){
         this.expression = expression;
     }
     
@@ -22,7 +23,7 @@ public class PrintFunctionCall extends ASTNode {
     @Override
     String toString(String indent) {
         StringBuilder sb = new StringBuilder();
-        sb.append(indent).append("└── Imprime expressión").append("\n");
+        sb.append(indent).append("└── Lee expressión").append("\n");
         sb.append(expression.toString(indent.replace("|", " ") + "    "));
         
         return sb.toString();
